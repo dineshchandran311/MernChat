df40b46675c4d75625be1cd4bf9468147a82e380
Dinesh Chandran <dineshchandran311@gmail.com>
2021-12-12 20:43:24 +0530

    Chat body header design

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,98 @@
+import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react';
+import './Chat.css'
 
 function Chat() {
     return (
         <div className='chat'>
-            
+            <div className='chat__header'>
+                <div className='chat__avatar'>
+                    <Avatar/>
+                </div>
+                <div className='chat__headerInfo'>
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className='chat__headerRight'>
+                    <IconButton>
+                        <SearchOutlined/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className='chat__body'>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message chat__reciever">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message chat__reciever">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message chat__reciever">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className='chat__name'>Raj</span>
+                    This is a message
+                    <span className='chat__timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
         </div>
     )
 }
